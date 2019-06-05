89589c0058df7e7a07234b629c8b7db85972bb21
PNUHC <luchunabf@163.com>
2019-06-05 17:21:06 +0800

    第一个C++程序-PNUHC

---
+++ test_6_5_firstC++/test_6_5_firstC++/FirstC++.cpp
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<iostream>
+//#include<process.h>
+//using namespace std;
+//int main()
+//{
+//	cout << "hello world" << endl;
+//	system("pause");
+//	return 0;
+//}
+
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+	system("pause");
+	return 0;
+}
