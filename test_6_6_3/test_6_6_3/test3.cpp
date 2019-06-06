e1a93b6f29314243b99c77e76e11bff0712b5045
PNUHC <luchunabf@163.com>
2019-06-06 17:08:06 +0800

    日常练习-PNUHC

---
+++ test_6_6_3/test_6_6_3/test3.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	double b;
+	char c;
+
+	cin >> a;
+	cin >> b >> c;
+
+	cout << a << endl;
+	cout << b << " " << c << endl;
+
+	system("pause");
+	return 0;
+}
