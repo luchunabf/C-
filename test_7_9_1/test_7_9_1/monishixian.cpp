211b538802577fe188dfcf240510014b5df81bd1
PNUHC <luchunabf@163.com>
2019-07-10 12:22:10 +0800

    stack,queue,priority_queue-PNUHC

---
+++ test_7_9_1/test_7_9_1/monishixian.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "stack.h"
+#include "queue.h"
+#include "priority_queue.h"
+int main()
+{
+	TestStack1();
+	TestStack2();
+	TestQueue1();
+	TestQueue2();
+	TestPriority1();
+	return 0;
+}
