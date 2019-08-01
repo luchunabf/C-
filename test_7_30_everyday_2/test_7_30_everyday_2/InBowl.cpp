926d13b32c75459ea3f42c3c981f30ae043360a0
PNUHC <luchunabf@163.com>
2019-08-01 23:10:26 +0800

    Inbowl-PNUHC

--- test_7_30_everyday_2/test_7_30_everyday_2/InBowl.cpp
+++ test_7_30_everyday_2/test_7_30_everyday_2/InBowl.cpp
@@ -11,13 +11,30 @@ bool InBowl(double n, double r)
 }
 int main()
 {
-	int n, r;
+	int n, r;//int 是问题所在，将 int 改为 double即可通过编译
 	while (cin >> n >> r)
 	{
 		if (InBowl((double)n, (double)r))
 			cout << "Yes" << endl;
 		else
 			cout << "No" << endl;
+	}
+	return 0;
+}
+
+
+//正确答案：
+#include <iostream>
+using namespace std;
+int main()
+{
+	double n, r;
+	while (cin >> n >> r)
+	{
+		if (3.14 * 2 * r < n)
+			cout << "No" << endl;
+		else
+			cout << "Yes" << endl;
 	}
 	return 0;
 }
